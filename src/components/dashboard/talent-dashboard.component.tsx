4b8adc0e85970cbcde83fa2f9c17c4dc1a348a8b
Chase <chase-fegenbush@uiowa.edu>
2018-10-01 14:47:21 -0400

    added talent dashboard container

---
+++ src/components/dashboard/talent-dashboard.component.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default class TalentDashboard extends React.Component {
+
+    public render() {
+        return (
+            <div>
+                <p>Talent Dashboard</p>
+            </div>
+        )
+    }
+}
