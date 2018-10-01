014c3b37f7881b37e5424ecca1531e0449b95725
James Rho LG laptop <jrho@bgsu.edu>
2018-10-01 15:47:14 -0400

    pulled

--- src/components/dashboard/talent-dashboard.component.tsx
+++ src/components/dashboard/talent-dashboard.component.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import {DashboardTilesContainer} from './dashboard-tiles/dashboard-tiles.component';
+import {}
 
 export default class TalentDashboard extends React.Component {
 
