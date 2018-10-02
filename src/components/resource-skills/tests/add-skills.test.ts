4a34090fdc2ff8164fde7c5090529ab0ee8b64a7
haus20xx <kennysmod@gmail.com>
2018-10-02 10:28:58 -0400

    Fixed test for AddSkillsComponent

--- src/components/resource-skills/tests/add-skills.test.ts
+++
@@ -1,9 +0,0 @@
-// import * as React from 'react';
-// import {shallow} from 'enzyme';
-import AddSkillsComponent from '../add-skills.component';
-
-describe( AddSkillsComponent, () => {
-    it('should submit the form', () => {
-      //  const wrapper = shallow(AddSkillsComponent);
-    });
-});
