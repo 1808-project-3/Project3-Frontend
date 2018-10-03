bb1739ee80e77f727673f6dfaa89cdc484171915
billyjb3 <billyjbrewer3@gmail.com>
2018-10-03 09:14:42 -0400

    layout created

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import {NavProfileText} from './nav-profile-text.component';
 import {NavProfileImage} from './nav-profile-image.component';
 import {NavLabel} from './nav-label.compoennt';
-import {SearchBar} from './nav-search-bar.component';
+// import {SearchBar} from './nav-search-bar.component';
 import {NavLink} from './nav-link.component';
 import {NavTitle} from './nav-title.component';
 
@@ -16,7 +16,7 @@ export class AppNav extends React.Component
       <div className="navbar">
         <NavTitle>TALENT PORTAL</NavTitle>
         <NavLabel>Search</NavLabel>
-        <SearchBar/>
+        {/* <SearchBar/> */}
         <NavLink>Advanced Search</NavLink>
         <NavProfileText/>
         <NavProfileImage/>
