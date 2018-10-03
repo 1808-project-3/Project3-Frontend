2143ce3526e0e1d8225919d6f61b17a5b9d55d4f
Adrien Lindner <lindner.54@osu.edu>
2018-10-03 15:11:59 -0400

    Reducer works for most inputs

--- src/components/resource-skills/add-skills.component.tsx
+++ src/components/resource-skills/add-skills.component.tsx
@@ -84,9 +84,9 @@ class AddSkillsComponent extends React.Component<IProps, {}> {
                                     </Row>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">CERTIFICATIONS, IF ANY</Label>
+                                    <Label for="inputCertifications" lg={4} className="font-weight-bold">CERTIFICATIONS, IF ANY</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="certifications" id="" />
+                                        <Input type="text" name="certifications" id="inputCertifications" />
                                     </Col>
                                 </FormGroup>
                                 <Row>
@@ -97,49 +97,49 @@ class AddSkillsComponent extends React.Component<IProps, {}> {
                             </Col>
                             <Col className="pl-0">
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold"> CUSTOMER NAME</Label>
+                                    <Label for="inputCustomerName" lg={4} className="font-weight-bold"> CUSTOMER NAME</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="customerName" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="customerName" id="inputCustomerName" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">PROJECT ID</Label>
+                                    <Label for="inputProjectId" lg={4} className="font-weight-bold">PROJECT ID</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="projectId" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="projectId" id="inputProjectId" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold"> PROJECT NAME</Label>
+                                    <Label for="inputProjectName" lg={4} className="font-weight-bold">PROJECT NAME</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="projectName" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="projectName" id="inputProjectName" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">GRADE</Label>
+                                    <Label for="inputGrade" lg={4} className="font-weight-bold">GRADE</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="grade" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="grade" id="inputGrade" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">COMPETENCY TAGGING</Label>
+                                    <Label for="inputCompetencyTagging" lg={4} className="font-weight-bold">COMPETENCY TAGGING</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="select" name="competencyTagging" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="select" name="competencyTagging" id="inputCompetencyTagging" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">DURATION</Label>
+                                    <Label for="inputDate" lg={4} className="font-weight-bold">DURATION</Label>
                                     <Col lg={8} className="my-auto">
                                         <div className="d-flex justify-content-between">
                                             <DatePicker
-                                                onChange={() => null}
-                                                value={undefined}
+                                                onChange={(date) => this.props.updateResource({ name: "startDate", value: date })}
+                                                value={resource.project && resource.project.startDate}
                                                 calendarIcon={<IoMdCalendar />}
                                                 clearIcon={null as any}
                                             />
                                             <span>To</span>
                                             <DatePicker
-                                                onChange={() => null}
-                                                value={undefined}
+                                                onChange={(date) => this.props.updateResource({ name: "endDate", value: date })}
+                                                value={resource.project && resource.project.endDate}
                                                 calendarIcon={<IoMdCalendar />}
                                                 clearIcon={null as any}
                                             />
@@ -147,28 +147,28 @@ class AddSkillsComponent extends React.Component<IProps, {}> {
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">HCM SUPERVISOR ID</Label>
+                                    <Label for="inputSupervisorId" lg={4} className="font-weight-bold">HCM SUPERVISOR ID</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="supervisorId" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="supervisorId" id="inputSupervisorId" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">HCM SUPERVISOR NAME</Label>
+                                    <Label for="inputSupervisorName" lg={4} className="font-weight-bold">HCM SUPERVISOR NAME</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="supervisorName" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="supervisorName" id="inputSupervisorName" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">LOCATION</Label>
+                                    <Label for="inputLocation" lg={4} className="font-weight-bold">LOCATION</Label>
                                     <Col lg={8} className="my-auto">
-                                        <Input type="text" name="location" id="" />
+                                        <Input onChange={e => this.props.updateResource(e.target)} type="text" name="location" id="inputLocation" />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
-                                    <Label for="" lg={4} className="font-weight-bold">ATTACHMENTS (RESUME)</Label>
+                                    <Label for="inputResumes" lg={4} className="font-weight-bold">ATTACHMENTS (RESUME)</Label>
                                     <Col lg={8} className="my-auto">
                                         <Label className="btn btn-secondary mb-0">
-                                            <small>UPLOAD RESUME</small><Input type="file" accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf" name="attachments" id="" hidden />
+                                            <small>UPLOAD RESUME</small><Input type="file" accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf" name="resumes" id="inputResumes" hidden />
                                         </Label>
                                     </Col>
                                 </FormGroup>
--- src/reducers/add-skills.reducer.ts
+++ src/reducers/add-skills.reducer.ts
@@ -2,6 +2,7 @@ import { addSkillsTypes } from "../actions/resource-skills/add-skills.types";
 import { IAddSkillsState } from ".";
 import { Resource } from "../models/Resource";
 import { User } from "../models/User";
+import { Project } from "../models/Project";
 
 const initialState: IAddSkillsState = {
     resource: new Resource
@@ -25,6 +26,54 @@ export const addSkillsReducer = (state = initialState, action: any) => {
                     break;
                 case "aopCertified":
                     newResource.aupCertified = action.payload.id === "aop-certified-yes";
+                    newState.resource = newResource;
+                    break;
+                case "customerName":
+                    const newCustomerNameProject = new Project({ ...state.resource.project, customerName: action.payload.value });
+                    newResource.project = newCustomerNameProject;
+                    newState.resource = newResource;
+                    break;
+                case "projectId":
+                    const newIdProject = new Project({ ...state.resource.project, pId: action.payload.value });
+                    newResource.project = newIdProject;
+                    newState.resource = newResource;
+                    break;
+                case "projectName":
+                    const newNameProject = new Project({ ...state.resource.project, name: action.payload.value });
+                    newResource.project = newNameProject;
+                    newState.resource = newResource;
+                    break;
+                case "grade":
+                    newResource.grade = action.payload.value;
+                    newState.resource = newResource;
+                    break;
+                case "competencyTagging":
+                    newResource.compentencyTagging = action.payload.value;
+                    newState.resource = newResource;
+                    break;
+                case "supervisorId":
+                    const newSupervisorIdProject = new Project({ ...state.resource.project, supervisor: new User({ ...state.resource.project.supervisor, assocId: action.payload.value }) });
+                    newResource.project = newSupervisorIdProject;
+                    newState.resource = newResource;
+                    break;
+                case "supervisorName":
+                    const newSupervisorNameProject = new Project({ ...state.resource.project, supervisor: new User({ ...state.resource.project.supervisor, firstName: action.payload.value }) });
+                    newResource.project = newSupervisorNameProject;
+                    newState.resource = newResource;
+                    break;
+                case "location":
+                    const newLocationProject = new Project({ ...state.resource.project, location: action.payload.value });
+                    newResource.project = newLocationProject;
+                    newState.resource = newResource;
+                    break;
+                case "startDate":
+                    const newStartDateProject = new Project({ ...state.resource.project, startDate: action.payload.value });
+                    newResource.project = newStartDateProject;
+                    newState.resource = newResource;
+                    break;
+                case "endDate":
+                    const newEndDateProject = new Project({ ...state.resource.project, endDate: action.payload.value });
+                    newResource.project = newEndDateProject;
                     newState.resource = newResource;
                     break;
             }
