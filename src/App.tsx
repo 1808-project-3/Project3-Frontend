09607d8a542c79c7b178a9ecb8120e9b4306c1bf
billyjb3 <billyjbrewer3@gmail.com>
2018-10-03 11:10:59 -0400

    fixed some underlying issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './include/bootstrap';
 import './App.css';
 import {Layout} from './components/layout/layout.component';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { HomeComponent } from './components/home/home.component';
 import SignInComponent from './components/sign-in/sign-in.component';
 import { Provider } from 'react-redux';
 import { store } from './Store';
@@ -14,15 +13,12 @@ class App extends React.Component {
       <Provider store={store}>
         <BrowserRouter>
           <div id="within-root">
-             {/* <AppNav />  */}
-            {/* need to make it so nav bar wont load on sign in screen */}
             <div id="main-content-container">
               <Switch>
                 <Route path="/sign-in" component={SignInComponent} />
                 <Route component={this.wrappedRoutes} />
               </Switch>
             </div>
-
           </div>
         </BrowserRouter>
       </Provider>
@@ -31,14 +27,11 @@ class App extends React.Component {
 
   // these are the routes that include the nav/side bars.
   public wrappedRoutes = () => (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/home" component={HomeComponent} />
-          <Route component={HomeComponent}/>
-        </Switch>
-      </Layout>
-    </div>
+    <Layout>
+      <Switch>
+        
+      </Switch>
+    </Layout>
   )
 }
 
--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-// import { connect } from 'react-redux';
-// import { Navbar, NavbarBrand } from 'reactstrap';
 import {NavProfileText} from './nav-profile-text.component';
 import {NavProfileImage} from './nav-profile-image.component';
 import {NavLabel} from './nav-label.compoennt';
 import {SearchBar} from './nav-search-bar.component';
 import {NavLink} from './nav-link.component';
 import {NavTitle} from './nav-title.component';
+import './navStyles.css';
 
 export class AppNav extends React.Component
 {
