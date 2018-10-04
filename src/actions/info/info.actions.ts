e25855cc00e71b10dc036c21b0e7d5eff3fc2e41
alexvicuna <vicunaalejandro@gmail.com>
2018-10-04 09:24:38 -0400

    Added documentation

--- src/actions/info/info.actions.ts
+++ src/actions/info/info.actions.ts
@@ -48,6 +48,11 @@ export const getResourceList = (tableType: string) => (dispatch: any) => {
         })
 }
 
+/**
+ * This action will make a GET request for the list of projects that will be displayed on the
+ * project list table.
+ */
+
 export const getProjectList = () => (dispatch:any) => {
     axios.get('https://my-json-server.typicode.com/avicuna/talent-portal-mock/projects')
         .then(response => {
@@ -74,6 +79,12 @@ export const getProjectList = () => (dispatch:any) => {
         })
 }
 
+/**
+ * This action will update the tableType based on the passed in parameter.
+ * @param text
+ * This signifies the passed in tableType.
+ */
+
 export const updateTableType = (text: string) =>  {
     return {
         payload: {
@@ -82,6 +93,12 @@ export const updateTableType = (text: string) =>  {
         type: infoTypes.UPDATE_TABLE_TYPE
     }
 }
+
+/**
+ * This action updates the error message.
+ * @param text
+ * This signifies the passed in error message.
+ */
 
 export const updateError = (text: string) => {
     return {
@@ -92,6 +109,12 @@ export const updateError = (text: string) => {
 
     }
 }
+
+/**
+ * This action updates the viewRow variable.
+ * @param id
+ * This signifies the passed in row id.
+ */
 
 export const updateViewRow = (id: number) => {
     return {
