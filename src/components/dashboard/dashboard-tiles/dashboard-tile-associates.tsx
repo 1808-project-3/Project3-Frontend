c22a8fdd68bcbd0ef2ac50821c8f17576dfd0522
James Rho LG laptop <jrho@bgsu.edu>
2018-10-04 15:25:53 -0400

    merging

--- src/components/dashboard/dashboard-tiles/dashboard-tile-associates.tsx
+++ src/components/dashboard/dashboard-tiles/dashboard-tile-associates.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
 import { Card, Col,Row,CardFooter} from 'reactstrap';
 import {MdGroup} from 'react-icons/md/'
+import axios from 'axios';
 
-export default class AssociatesTile extends React.Component {
+export default class AssociatesTile extends React.Component<any,any> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            numOfAssociates: 0
+         
+      }
+    }
 
-    // displays total number of associates in db
-    // clicking this page opens the associates list page
+    public async componentDidMount() {
+        console.log('got here');
+        const res = await axios.get('http://localhost:8080/users',{headers: {"JWT": 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2Vycy9Uek1Vb2NNRjRwIiwiZXhwIjo2MjUxNjM3OTYwMCwidXNlcmlkIjoxMjM0NTYsInNjb3BlIjoic2VsZiBncm91cHMvdXNlcnMifQ.nD9kCwmbAIpFj__Qq_e2_XOkbBCe6zhXu713DoBOCjY’' }});
+        console.log('after fetch');
+        this.setState({numOfAssociates: res.data.length});
+        console.log(this.state.numOfAssociates);
+        
+    }
 
     public render() {
         return (
-
             <Card className="text-center">
                 <Row>
                 <Col md={2}>
                     <div className="skillgroup-skill-card-content">
-                        <p className="project-number-card">404</p>
-
+                        <p className="project-number-card">{this.state.numOfAssociates}</p>
                     </div>
                 </Col>
                 <Col md={6}>
