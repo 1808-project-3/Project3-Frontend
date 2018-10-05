1cd507fa78925daa57a447a890f7903a5d6dde43
Justin Massey <jmassey_1991@hotmail.com>
2018-10-05 18:26:37 -0400

    implemented search result page with mock data

--- src/components/search-result/search-result.component.tsx
+++ src/components/search-result/search-result.component.tsx
@@ -1,18 +1,70 @@
 import * as React from 'react';
 import { IState } from '../../reducers';
 import { connect } from 'react-redux';
+import { Table, Label } from 'reactstrap';
 
 
 export class SearchResultComponent extends React.Component<any, {}> {
 
+    public mockResults = [
+        {
+            Actions: 'View/ Edit',
+            AssociateName: 'John Wells',
+            Certification: 'Cert Details',
+            Grade: 'M',
+            ID: 348847,
+            ProjectName: 'Project Name',
+
+        },
+        {
+            Actions: 'View/ Edit',
+            AssociateName: 'Bob Man',
+            Certification: 'Cert Details',
+            Grade: 'SA',
+            ID: 948347,
+            ProjectName: 'Project Name',
+
+        },
+        {
+            Actions: 'View/ Edit',
+            AssociateName: 'Blake Kruppa',
+            Certification: 'Cert Details',
+            Grade: 'M',
+            ID: 69_420,
+            ProjectName: 'Talent Portal Server Dream',
+
+        }
+    ]
+
+
     public render() {
         return (
-            <div>Search Result Component
-                <div>
-                    <h1>{this.props.register.userID}</h1>
-                    <h1>{this.props.signIn.credentials.userId}</h1>
-                    <h1>{this.props.signIn.credentials.pass}</h1>
-                </div>
+            <div>
+                <Label className="search-result-label"><small className='font-weight-bold'>SEARCH RESULTS</small></Label>
+            <Table className='search-result-table'>
+                <thead>
+                    <tr>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>ASSOCIATE NAME</small></Label></th>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>ID</small></Label></th>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>CERTIFICATION</small></Label></th>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>PROJECT DETAILS</small></Label></th>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>GRADE</small></Label></th>
+                        <th><Label className="search-result-label"><small className='font-weight-bold'>DETAILS</small></Label></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.mockResults.map((associate: any) =>
+                      <tr key={associate.ID}>
+                        <td className="search-result-label"><small>{associate.AssociateName}</small></td>
+                        <td className="search-result-label"><small>{associate.ID}</small></td>
+                        <td className="search-result-label"><small>{associate.Certification}</small></td>
+                        <td className="search-result-label"><small>{associate.ProjectName}</small></td>
+                        <td className="search-result-label"><small>{associate.Grade}</small></td>
+                        <td className="search-result-label"><small>{associate.Actions}</small></td>
+                      </tr>
+                    )}
+                </tbody>
+            </Table>
             </div>
         );
     }
