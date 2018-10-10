5040908ab15e65f344a9dd28c373ea3c9956cf5b
Adrien Lindner <lindner.54@osu.edu>
2018-10-10 10:22:38 -0400

    Fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import './App.css';
 import { Layout } from './components/layout/layout.component';
 import RegisterComponent from './components/register/register.component';
@@ -10,13 +10,14 @@ import SignInComponent from './components/sign-in/sign-in.component';
 import './include/bootstrap';
 import { store } from './Store';
 import TalentDashboard from './components/dashboard/talent-dashboard.component';
+import history from './history'
 
 class App extends React.Component {
 
   public render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <Router history={history}>
           <div id="main-content-container">
             <Switch>
               <Route path="/sign-in" component={SignInComponent} />
@@ -26,7 +27,7 @@ class App extends React.Component {
             </Switch>
           </div>
 
-        </BrowserRouter>
+        </Router>
       </Provider>
     );
   }
@@ -35,7 +36,7 @@ class App extends React.Component {
   // add the paths that the sidebar buttons will lead to in sidebar component; place paths in the link attribute starting with '/'
   // the names used in the route paths below will be what is displayed in the breadcrumbs so keep that in mind when naming routes.
   public wrappedRoutes = (props: any) => (
-    <Layout location={props.location} history={props.history}>
+    <Layout location={props.location} history={history}>
       <Switch>
         <Route path="/home/resources/search-results" component={SearchResultComponent} />
         <Route path="/home/add-skills" component={ResourceSkillDisplayComponent} />
