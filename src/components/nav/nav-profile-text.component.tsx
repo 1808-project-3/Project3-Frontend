ad9d6b9d0c613a80028eaf26a272ee46b1feed35
Justin Massey <jmassey_1991@hotmail.com>
2018-10-11 16:26:32 -0400

    added current time to the nav bar last login

--- src/components/nav/nav-profile-text.component.tsx
+++ src/components/nav/nav-profile-text.component.tsx
@@ -3,13 +3,28 @@ import './navStyles.css';
 
 export class NavProfileText extends React.Component
 {
+    public date = new Date();
+    public checkMinutes = () =>  {
+        if(this.date.getMinutes() < 10) {
+            const newMinutes = 0 + this.date.getMinutes();
+            return newMinutes;
+         }
+         else{ return this.date.getMinutes(); }
+    }
+    public checkHours = () =>  {
+        if(this.date.getHours() > 12) {
+            const newHours = +this.date.getHours() - 12;
+            return newHours;
+         }
+         else{ return this.date.getHours(); }
+    }
     public render()
     {
         const name = localStorage.getItem('userName');
         return (
             <div className="profileText">
                 <div className="profileName"> Welcome {name}</div>
-                <div className="profileLogged">Last Logged In 12:30</div>
+                <div className="profileLogged">Last Logged In {this.checkHours()}:{this.checkMinutes()}</div>
             </div>
         )
     }
