05ef6bdd2c32cb5c51a1bf5aa13951a8738c22db
Justin Massey <jmassey_1991@hotmail.com>
2018-10-11 15:44:52 -0400

    added logout button that clears all states

--- src/components/nav/nav-profile-text.component.tsx
+++ src/components/nav/nav-profile-text.component.tsx
@@ -5,9 +5,10 @@ export class NavProfileText extends React.Component
 {
     public render()
     {
+        const name = localStorage.getItem('userName');
         return (
             <div className="profileText">
-                <div className="profileName">Welcome John</div>
+                <div className="profileName"> Welcome {name}</div>
                 <div className="profileLogged">Last Logged In 12:30</div>
             </div>
         )
