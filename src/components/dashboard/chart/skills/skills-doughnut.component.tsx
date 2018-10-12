4d5b0cddfd4441d98e97030363c53878ef6b4aec
Adrien Lindner <lindner.54@osu.edu>
2018-10-12 14:43:44 -0400

    Fixed crash

--- src/components/dashboard/chart/skills/skills-doughnut.component.tsx
+++ src/components/dashboard/chart/skills/skills-doughnut.component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Row, Col, Container, Card} from 'reactstrap';
+import { Row, Col, Container, Card } from 'reactstrap';
 import { Doughnut } from 'react-chartjs-2';
 import { apiClient } from 'src/axios/api-client';
 import Scrollbars from 'react-custom-scrollbars';
@@ -10,31 +10,31 @@ interface IProps {
 
 export default class SkillDoughnut extends React.Component<IProps, any> {
 
-    constructor(props:any) {
+    constructor(props: any) {
         super(props);
         this.state = {
             selectedSkillGroup: '',
             skillChart: {
                 data: {
-                    datasets: [{                                  
-                    backgroundColor: [
-                      '#FF6384', // red
-                      '#36A2EB', // blue
-                      '#FFCE56', // yellow
-                      '#24e216', // green
-                      '#3713d6' // purple
-                    ],
-                    borderColor: 'rgba(255,255,255,0.54)',
-                    data: [],                  
-                    hoverBackgroundColor: [
-                      '#FF6384', // red
-                      '#36A2EB', // blue
-                      '#FFCE56', // yellow
-                      '#24e216', // green
-                      '#3713d6' // purple
-                    ],
-                  }],              
-                  labels: []                              
+                    datasets: [{
+                        backgroundColor: [
+                            '#FF6384', // red
+                            '#36A2EB', // blue
+                            '#FFCE56', // yellow
+                            '#24e216', // green
+                            '#3713d6' // purple
+                        ],
+                        borderColor: 'rgba(255,255,255,0.54)',
+                        data: [],
+                        hoverBackgroundColor: [
+                            '#FF6384', // red
+                            '#36A2EB', // blue
+                            '#FFCE56', // yellow
+                            '#24e216', // green
+                            '#3713d6' // purple
+                        ],
+                    }],
+                    labels: []
                 },
                 options: {
                     legend: {
@@ -43,27 +43,27 @@ export default class SkillDoughnut extends React.Component<IProps, any> {
                 }
             },
             skillGroupChart: {
-                data: {   
-                    datasets: [{                                      
-                      backgroundColor: [
-                        '#FF6384', // red
-                        '#36A2EB', // blue
-                        '#FFCE56', // yellow
-                        '#24e216', // green
-                        '#3713d6' // purple
-                      ],
-                      borderColor: 'rgba(255,255,255,0.54)',
-                      data: [],                    
-                      hoverBackgroundColor: [
-                        '#FF6384', // red
-                        '#36A2EB', // blue
-                        '#FFCE56', // yellow
-                        '#24e216', // green
-                        '#3713d6' // purple
-                      ],
-                    }],                      
-                      labels: [],                    
-                  },
+                data: {
+                    datasets: [{
+                        backgroundColor: [
+                            '#FF6384', // red
+                            '#36A2EB', // blue
+                            '#FFCE56', // yellow
+                            '#24e216', // green
+                            '#3713d6' // purple
+                        ],
+                        borderColor: 'rgba(255,255,255,0.54)',
+                        data: [],
+                        hoverBackgroundColor: [
+                            '#FF6384', // red
+                            '#36A2EB', // blue
+                            '#FFCE56', // yellow
+                            '#24e216', // green
+                            '#3713d6' // purple
+                        ],
+                    }],
+                    labels: [],
+                },
                 options: {
                     legend: {
                         labels: {
@@ -71,7 +71,7 @@ export default class SkillDoughnut extends React.Component<IProps, any> {
                             fontSize: 14,
                             padding: 30
                         },
-                        onClick: (e:any, legendItem:any) => {
+                        onClick: (e: any, legendItem: any) => {
                             console.log(legendItem.text);
                             this.setSkillChart(legendItem.text);
                         },
@@ -79,78 +79,79 @@ export default class SkillDoughnut extends React.Component<IProps, any> {
                     }
                 }
             },
-            skillGroups: []            
+            skillGroups: []
         };
-      }
-
-    public setSkillChart(skillGroup: string) { 
-        // get the data from the state and place the labels/values into the sub chart of skills for the selected skill group
-        const selectedSkillGroup = this.state.skillGroups.filter((group:any) => group.skillGroupName === skillGroup);        
-
-        const skillLabels = selectedSkillGroup[0].skills.map((val:any) => val.skillName);
-        const skillValues = selectedSkillGroup[0].skills.map((val:any) => val.totalSkill);
-        
-        this.setState({selectedSkillGroup: skillGroup});        
-        this.setState({skillChart: {data: {datasets: [{data: skillValues}], labels: skillLabels}}});
     }
-    
+
+    public setSkillChart(skillGroup: string) {
+        // get the data from the state and place the labels/values into the sub chart of skills for the selected skill group
+        const selectedSkillGroup = this.state.skillGroups.filter((group: any) => group.skillGroupName === skillGroup);
+        if (selectedSkillGroup) {
+            const skillLabels = selectedSkillGroup[0].skills.map((val: any) => val.skillName);
+            const skillValues = selectedSkillGroup[0].skills.map((val: any) => val.totalSkill);
+
+            this.setState({ selectedSkillGroup: skillGroup });
+            this.setState({ skillChart: { data: { datasets: [{ data: skillValues }], labels: skillLabels } } });
+        }
+    }
+
     public async componentDidMount() {
-        const list: any[] = [];                
-        
-        const res = this.props;        
+        const list: any[] = [];
+
+        const res = this.props;
         const res1 = await apiClient.get('skills');
         const res2 = await apiClient.get('skill-group');
 
-         // loop through groups
-         res2.data.forEach((group:any, i:any) => {
-            const listObj = {            
+        // loop through groups
+        res2.data.forEach((group: any, i: any) => {
+            const listObj = {
                 skillGroupName: "",
                 skills: [],
-                totalSkillGroup: 0            
+                totalSkillGroup: 0
             }
-            
-            list.push(listObj); 
+
+            list.push(listObj);
             list[i].skillGroupName = group.groupName;
-            
+
             // loop through skills      
-            res1.data.forEach((skill:any, k:any) => { 
+            res1.data.forEach((skill: any, k: any) => {
                 const skillObj = {
                     skillName: "",
                     totalSkill: 0
-                }               
-                
+                }
+
                 if (skill.groupId === group.id) {
-                    skillObj.skillName = skill.skillName;                                        
+                    skillObj.skillName = skill.skillName;
 
                     // loop through users to check if they have that skill
-                    res.users.forEach((user:any) => {
-                        user.userSkills.forEach((userSkill:any) => {
+                    res.users.forEach((user: any) => {
+                        user.userSkills.forEach((userSkill: any) => {
                             if (userSkill.skillId === skill.id) {
                                 skillObj.totalSkill++
                                 list[i].totalSkillGroup++
                             }
-                        })   
+                        })
                     })
                     list[i].skills.push(skillObj);
                 }
             })
         })
         // place the list into the state for a ref to the data when another skill group is selected
-        this.setState({skillGroups: list});
+        this.setState({ skillGroups: list });
 
         // get skill group labels and values
         const skillGroupLabels = list.map(val => val.skillGroupName);
         const skillGroupValues = list.map(val => val.totalSkillGroup);
 
         // put data into skill group chart                               
-        this.setState({ skillGroupChart: {data: {datasets: [{data: skillGroupValues}], labels: skillGroupLabels}}});
+        this.setState({ skillGroupChart: { data: { datasets: [{ data: skillGroupValues }], labels: skillGroupLabels } } });
 
         // get skill group with highest num of associates and set the subchart
-        const max = list.reduce((prev:any, current:any) => 
+        const max = list.reduce((prev: any, current: any) =>
             prev.totalSkillGroup > current.totalSkillGroup ? prev : current)
 
         // call method to set the sub chart of skills
-        this.setSkillChart(max.skillGroupName);                
+        this.setSkillChart(max.skillGroupName);
     }
 
     public padZero(num: any) {
@@ -159,52 +160,52 @@ export default class SkillDoughnut extends React.Component<IProps, any> {
 
     public render() {
         return (
-                <Container>
-                    <Row style={{height: "27.25vw"}}>
-                        <Col md={7}>                        
-                            <div className="skill-group-chart">
-                                <Doughnut data={this.state.skillGroupChart.data} options={this.state.skillGroupChart.options}/>                              
-                            </div>
-                        </Col>
-                        <Col md={5}>                        
-                            <div className="skill-chart">
-                                <Doughnut data={this.state.skillChart.data} options={this.state.skillChart.options} />                        
-                            </div>
-                        </Col>
-                    </Row>                                        
-                    <Row className="skill-border">
-                        <Col md={6} style={{paddingLeft:'16%'}}>
-                            <p>TOTAL ASSOCIATES</p>
-                        </Col>
-                        <Col md={6} style={{paddingLeft: '5%', textAlign: "center"}}>                        
-                            <p className="chart-selected-skill-group">{this.state.selectedSkillGroup}</p>
-                        </Col>
-                    </Row>                                                            
-                    <Row style={{marginTop: "2%"}}>
-                        <Col md={6}>
-                            <p className="cards-selected-skill-group">{this.state.selectedSkillGroup}</p>
-                        </Col>
-                    </Row>
-                    <Scrollbars style={{height: "20vh"}}>
-                        <div className="skill-cards-container">                                                
-                            <Row>
-                            {this.state.skillChart.data.datasets[0].data.map((val:any, index: any) => (
+            <Container>
+                <Row style={{ height: "27.25vw" }}>
+                    <Col md={7}>
+                        <div className="skill-group-chart">
+                            <Doughnut data={this.state.skillGroupChart.data} options={this.state.skillGroupChart.options} />
+                        </div>
+                    </Col>
+                    <Col md={5}>
+                        <div className="skill-chart">
+                            <Doughnut data={this.state.skillChart.data} options={this.state.skillChart.options} />
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="skill-border">
+                    <Col md={6} style={{ paddingLeft: '16%' }}>
+                        <p>TOTAL ASSOCIATES</p>
+                    </Col>
+                    <Col md={6} style={{ paddingLeft: '5%', textAlign: "center" }}>
+                        <p className="chart-selected-skill-group">{this.state.selectedSkillGroup}</p>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: "2%" }}>
+                    <Col md={6}>
+                        <p className="cards-selected-skill-group">{this.state.selectedSkillGroup}</p>
+                    </Col>
+                </Row>
+                <Scrollbars style={{ height: "20vh" }}>
+                    <div className="skill-cards-container">
+                        <Row>
+                            {this.state.skillChart.data.datasets[0].data.map((val: any, index: any) => (
                                 <Col sm={3} key={index}>
                                     <Card className="skill-card">
                                         <div className="skill-card-content">
                                             <p className="skill-card-num-associates">{this.padZero(val)}</p>
-                                            <div className="skill-card-text">                                            
+                                            <div className="skill-card-text">
                                                 <p className="skill-card-associates-in">Associates In</p>
                                                 <p className="skill-card-skill-name">{this.state.skillChart.data.labels[index]}</p>
                                             </div>
                                         </div>
                                     </Card>
                                 </Col>
-                            ))}    
-                            </Row>                        
-                        </div>
-                    </Scrollbars>
-                </Container>
+                            ))}
+                        </Row>
+                    </div>
+                </Scrollbars>
+            </Container>
         )
     }
 
